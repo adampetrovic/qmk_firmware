64fae46a7a888573138963059f84a06e75e81061
Adam Petrovic <apetrovic@atlassian.com>
2024-07-27 16:14:09 +1000

    add more keycodes

--- keyboards/kinesis/keymaps/adampetrovic/keymap.c
+++ keyboards/kinesis/keymaps/adampetrovic/keymap.c
@@ -20,6 +20,15 @@
 #define LAYER1 1
 
 
+#define L1_T_SCLN  LT(1, KC_SCLN)
+
+// Rectangle Keycodes
+#define RECT_FS     LOPT(LCMD(KC_F))  // Rectangle - Fullscreen
+#define RECT_LH     LOPT(LCMD(KC_LEFT))  // Rectangle - Left Half
+#define RECT_RH     LOPT(LCMD(KC_RIGHT))  // Rectangle - Right Half
+#define RECT_TH     LOPT(LCMD(KC_UP))  // Rectangle - Top Half
+#define RECT_BH     LOPT(LCMD(KC_DOWN))  // Rectangle - Bottom Half
+
 /****************************************************************************************************
 *
 * Keymap: Default Layer in Qwerty
@@ -40,7 +49,7 @@
 *                                        ,-------------.         ,-------------.
 *                                        | Ctrl | Cmd  |         | ROpt  | Meh |
 *                                 ,------|------|------|         |------+------+------.
-*                                 |      |      | Home |         | PgUp |      |      |
+*                                 |      |      | Lyr1 |         | PgUp |      |      |
 *                                 | Space| Bkspc|------|         |------|Delete| Enter|
 *                                 |      |      | End  |         | PgDn |      |      |
 *                                 `--------------------'         `--------------------'
@@ -48,14 +57,14 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [QWERTY] = LAYOUT(
-           KC_CAPS, KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,KC_F6  ,KC_F7  ,KC_F8,    KC_F9  ,KC_F10 ,KC_F11 ,KC_F12 ,KC_PSCR ,KC_SCRL  ,KC_PAUS, MO(1), QK_BOOT,
+           KC_CAPS, KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,KC_F6  ,KC_F7  ,KC_F8,    KC_F9  ,KC_F10 ,KC_F11 ,KC_F12 ,KC_PSCR ,KC_SCRL  ,KC_PAUS, KC_NO, QK_BOOT,
            KC_EQL, KC_1   ,KC_2   ,KC_3   ,KC_4   ,KC_5   ,     KC_6   ,KC_7   ,KC_8   ,KC_9   ,KC_0   ,KC_MINS,
            KC_TAB, KC_Q   ,KC_W   ,KC_E   ,KC_R   ,KC_T   ,     KC_Y   ,KC_U   ,KC_I   ,KC_O   ,KC_P   ,KC_BSLS,
-           KC_ESC,KC_A   ,KC_S   ,KC_D   ,KC_F   ,KC_G   ,     KC_H   ,KC_J   ,KC_K   ,KC_L   ,LCMD_T(KC_SCLN),KC_QUOT,
+           KC_ESC, KC_A   ,KC_S   ,KC_D   ,KC_F   ,KC_G   ,     KC_H   ,KC_J   ,KC_K   ,KC_L   ,L1_T_SCLN,KC_QUOT,
            KC_LSFT,KC_Z   ,KC_X   ,KC_C   ,KC_V   ,KC_B   ,     KC_N   ,KC_M   ,KC_COMM,KC_DOT ,KC_SLSH,KC_RSFT,
                    KC_GRV ,KC_INS ,KC_LEFT,KC_RGHT,             KC_UP  ,KC_DOWN,KC_LBRC,KC_RBRC,
 			   KC_LCTL,KC_LCMD,           KC_RALT,KC_MEH,
-                                    KC_ROPT,           KC_PGUP,
+                                    LT(1, LCMD(KC_ENT)),           KC_PGUP,
                            KC_SPC,KC_BSPC,KC_END,           KC_PGDN,KC_DEL,KC_ENT
     ),
 
@@ -64,12 +73,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // as printed on the Kinesis Advantage 2.
 [LAYER1] = LAYOUT(
 
-           KC_WAKE,  KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO,    KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_MUTE ,KC_VOLD  ,KC_VOLU, KC_TRNS, KC_NO  ,
-           KC_NO  ,  KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,   KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,
-           KC_NO  ,  KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,   KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,
-           KC_NO  ,  KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,   KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,
-           KC_NO  ,  KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,   KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,
-                   KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,             KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,
+           KC_NO ,  KC_NO  ,KC_NO  ,KC_NO  , KC_NO  ,KC_NO  ,   KC_NO  ,KC_NO  ,KC_NO,  KC_NO  ,KC_NO  ,KC_NO  , KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+           KC_NO ,  KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,   KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,
+           KC_NO ,  KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,   KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,
+           KC_NO ,  KC_NO  ,KC_NO  ,KC_NO  ,RECT_FS,KC_NO  ,   KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,
+           KC_NO ,  KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,   KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,KC_NO  ,
+                   KC_NO  ,KC_NO  ,RECT_LH  ,RECT_RH  ,             RECT_TH  ,RECT_BH  ,KC_NO  ,KC_NO  ,
 			   KC_NO  ,KC_NO  ,           KC_NO  ,KC_NO  ,
                                     KC_NO  ,           KC_NO  ,
                            KC_NO  ,KC_NO  ,KC_NO  ,           KC_NO  ,KC_NO    ,KC_NO
